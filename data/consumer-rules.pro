2e4a0bea6c2745a6f96ed5bbfa02877f0ce96ae6
Nicolas Szkwir <nicoszkwir@gmail.com>
2024-05-11 11:34:37 +0200

    Create project structure

---
+++ data/consumer-rules.pro
@@ -0,0 +1,1 @@
+-keep class com.spitzer.data.**.dto.** { *; }
---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "recipes"
+include(":app")
+include(":ui:recipe")
+include(":designsystem")
+include(":data")
+include(":domain")
+include(":model")
